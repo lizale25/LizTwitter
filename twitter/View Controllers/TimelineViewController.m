d3b29a03ce9a44c712fb478cff5e9464ad9307f6
Lizbeth Alejandra Gonzalez <lizgonzalez@fb.com>
2018-07-04 23:07:40 -0700

    logout button works

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -12,6 +12,8 @@
 #import "TweetCell.h"
 #import <UIImageView+AFNetworking.h>
 #import "ComposeViewController.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController () <ComposeViewControllerDelegate, UITableViewDataSource, UITableViewDelegate>
 
@@ -95,6 +97,14 @@
     [self.tableView reloadData];
 }
 
+- (IBAction)logOut:(id)sender {
+   AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    [[APIManager shared] logout];
+    
+}
 
 
 // Makes a network request to get updated data
